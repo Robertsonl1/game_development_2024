9120dd7386ee1925768fc4d4c3e995b3de45c403
AHeavyOfCulture <65954164+AHeavyOfCulture@users.noreply.github.com>
2024-02-08 17:49:19 +1300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fmt::{Display, Formatter};
+
 fn main() {
-    println!("Hello, world!");
+    let ranga: u8 = 129;
+    let mut player: Player = Player{ name: "Joe".to_string(), health: 0 };
+    player.take_damage();
+    let player_2 = Player::new("Bob".to_string());
+
+    println!("Value is {player}");
+}
+#[derive(Debug)]
+struct Player{
+    name: String,
+    health: i8,
+
+}
+impl Display for Player{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.name, self.health)
+    }
+}
+impl Player{
+    fn new(name: String) -> Player{
+        return Player{ name: name, health: 5 }
+    }
+    fn take_damage(&mut self) {
+        self.health -= 1;
+
+
+    }
+
 }
