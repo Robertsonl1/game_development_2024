fc8f56878161bc078b69e0011388dc21364616fe
AHeavyOfCulture <65954164+AHeavyOfCulture@users.noreply.github.com>
2024-02-13 19:01:53 +1300

    basic bevy project

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
-use std::fmt::{Display, Formatter};
+use bevy::prelude::*;
 
 fn main() {
-    let ranga: u8 = 129;
-    let mut player: Player = Player{ name: "Joe".to_string(), health: 0 };
-    player.take_damage();
-    let player_2 = Player::new("Bob".to_string());
+    App::new()
+        .add_plugins((DefaultPlugins,))
 
-    println!("Value is {player}");
-}
-#[derive(Debug)]
-struct Player{
-    name: String,
-    health: i8,
-
-}
-impl Display for Player{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {}", self.name, self.health)
-    }
-}
-impl Player{
-    fn new(name: String) -> Player{
-        return Player{ name: name, health: 5 }
-    }
-    fn take_damage(&mut self) {
-        self.health -= 1;
-
-
-    }
-
+        .run();
 }
