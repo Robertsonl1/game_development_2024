be59e0b61df393805b5cbb8de48f05677fbd97a3
AHeavyOfCulture <65954164+AHeavyOfCulture@users.noreply.github.com>
2024-02-21 19:45:27 +1300

    tutorial

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,5 @@
+use bevy::core_pipeline::bloom::{BloomCompositeMode, BloomSettings};
+use bevy::core_pipeline::tonemapping::Tonemapping;
 use bevy::prelude::*;
 
 const CAMERA_DISTANCE: f32 = 80.0;
@@ -11,8 +13,14 @@ impl Plugin for CameraPlugin {
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
+    commands.spawn((Camera3dBundle {
         transform: Transform::from_xyz(0.0, CAMERA_DISTANCE, 0.0).looking_at(Vec3::ZERO, Vec3::Z),
+        tonemapping: Tonemapping::BlenderFilmic,
+        camera: Camera {hdr: true, ..default()},
         ..default()
-    });
+    }, BloomSettings {
+        composite_mode: BloomCompositeMode::Additive,
+        intensity: 0.05,
+        ..default()
+    }));
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ mod movement;
 mod debug;
 mod spaceship;
 mod camera;
+mod asteroids;
+mod assetloader;
+mod collision_detection;
+mod despawn;
 
 use bevy::prelude::*;
 use debug::DebugPlugin;
 use movement::MovementPlugin;
 use spaceship::SpaceshipPlugin;
 use camera::CameraPlugin;
+use assetloader::AssetLoaderPlugin;
+use asteroids::AsteroidPlugin;
+use collision_detection::CollisionDetectionPlugin;
+use despawn::DespawnPlugin;
 
 fn main() {
     App::new()
         // Bevy built-ins
-        .insert_resource(ClearColor(Color::rgb(0.7, 0.4, 0.0)))
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(AmbientLight {
             color: Color::default(),
             brightness: 0.75,
@@ -22,9 +30,13 @@ fn main() {
 
         .add_plugins(SpaceshipPlugin)
         // User configured plugins.
+        .add_plugins(AssetLoaderPlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(DebugPlugin)
         .add_plugins(DefaultPlugins)
+        .add_plugins(AsteroidPlugin)
+        .add_plugins(CollisionDetectionPlugin)
+        .add_plugins(DespawnPlugin)
         .run();
 }
